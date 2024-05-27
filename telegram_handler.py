f9346420d9cbb19485e70112fc4475812899a21f
victormorozov1 <vismo@bk.ru>
2024-05-27 17:29:47 +0300

    add infinity retries to TelegramHandler

--- telegram_handler.py
+++ telegram_handler.py
@@ -1,11 +1,19 @@
 import logging
-from typing import Callable
+
+from telebot import TeleBot
+from telebot.apihelper import ApiException
+from tenacity import retry, RetryError, wait_exponential
+
+logger = logging.getLogger('__main__')
 
 
 class TelegramHandler(logging.Handler):
-    def __init__(self, send_msg_func: Callable, *args, **kwargs):
+    def __init__(self, bot: TeleBot, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.send_msg_func = send_msg_func
+        self.bot = bot
 
+    @retry(wait=wait_exponential(max=60))
     def emit(self, record: logging.LogRecord) -> None:
-        self.send_msg_func(self.format(record))
+        # TelegramHandler для важных сообщений, поэтому бесконечные ретраи
+        # TODO: подумать, мб сделать это место более безопасным
+        self.bot.send_message('@abobafrompsu', self.format(record))
